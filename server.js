04deedce33a614b81eb9e7b79c1f6b1bb1919023
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-07-20 09:26:16 -0600

    added rollbar APM

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const path = require("path");
 // require rollbar below
+const Rollbar = require("rollbar"); //this is capitalized bc it is a class
 
 // create the Rollbar class below
+const rollbar = new Rollbar({
+  accessToken: "7ba59ceab6d94e5b9bc383b6b758e120", //this  is found in rollbar
+  captureUncaught: true, //anytime server has uncaught error, rollbar will display in log
+  captureUnhandledRejections: true,
+});
 
 const app = express();
 app.use(express.json());
@@ -10,7 +16,8 @@ let studentList = [];
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
-  // send rollbar some info
+  // send rollbar some info // happens everytime project is loaded
+  rollbar.info("html file served successfully");
 });
 
 app.post("/api/student", (req, res) => {
